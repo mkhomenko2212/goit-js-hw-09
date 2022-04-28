ab77f26818c29749752297dcb289fee99da3d7c6
mkhomenko2212 <mkhomenko2212@gmail.com>
2022-04-28 23:24:21 +0200

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const refs={ 
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+    bodyColorEl: document.querySelector("body"),
+}
 
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+refs.startBtn.addEventListener("click", () => {
+  timerId = setInterval(() => { refs.bodyColorEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+    
+    refs.startBtn.disabled = true;
+});
+
+
+refs.stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+refs.startBtn.disabled = false;
+});
