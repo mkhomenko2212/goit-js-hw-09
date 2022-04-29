19482a7985be8674a259493ef5c3bccd53f398b8
mkhomenko2212 <mkhomenko2212@gmail.com>
2022-04-29 14:10:20 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,7 +2,7 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
 import Notiflix from 'notiflix';
-import "notiflix/dist/notiflix-3.2.2.min.css";
+import "dist/notiflix-3.2.5.min.css" 
 
 
 const options = {
